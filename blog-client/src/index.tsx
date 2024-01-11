29f5f387bc463091d5f1736e9b1883c8dec236f0
faizttn <faijuddin@tothenew.com>
2024-01-11 21:09:45 +0530

    install pagination package

--- blog-client/src/index.tsx
+++ blog-client/src/index.tsx
@@ -3,21 +3,25 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { SnackbarProvider, closeSnackbar } from 'notistack';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <SnackbarProvider maxSnack={8} autoHideDuration={10000} action={(snackbarId) => (
+      <a href='#' onClick={() => closeSnackbar(snackbarId)}>Dismiss</a>
+    )}>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path='/*' element={<App />}></Route>
+          </Routes>
+        </Router>
+      </Provider>
+    </SnackbarProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
